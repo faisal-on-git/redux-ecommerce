8e2face49ed6005b5f338d18f810a540b85c0e80
faisal-work <faisal@rently.com>
2025-03-06 06:30:42 +0530

    added env based base url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const baseUrl = process.env.REACT_APP_ENV === "development" ? "https://zqqv728exd.execute-api.localhost.localstack.cloud:4566/prod" : "https://fakestoreapi.com"
+const baseUrl = process.env.REACT_APP_ENV === "development" ? "https://bs9m28vrpo.execute-api.localhost.localstack.cloud:4566/dev" : "https://fakestoreapi.com"
 
 export const apiInstance = axios.create({
     baseURL: baseUrl,
